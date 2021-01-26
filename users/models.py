c6d69476240e8bcfb3f054b69fb94c5fcb666286
Rakibul Hasan <six.rakib@gmail.com>
2021-01-26 23:15:45 +0600

    user model updated

--- users/models.py
+++ users/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
+from django.contrib.home.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['username']
+    username = models.CharField(blank=True, max_length=254,)
+    email = models.EmailField(blank=False, max_length=254, unique=True, verbose_name="Email Address",)
+    
+
