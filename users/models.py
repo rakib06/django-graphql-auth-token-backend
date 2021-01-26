d4b836addedbff1bbaec65af6a0fbe03295432ce
Rakibul Hasan <six.rakib@gmail.com>
2021-01-26 23:30:03 +0600

    database migration and migrate done

--- users/models.py
+++ users/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-from django.contrib.home.models import AbstractUser
-
+# Create your models here.
+from django.contrib.auth.models import AbstractUser
+ 
 class CustomUser(AbstractUser):
     EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['username']
-    username = models.CharField(blank=True, max_length=254,)
-    email = models.EmailField(blank=False, max_length=254, unique=True, verbose_name="Email Address",)
-    
-
+    REQUIRED_FIELDS = ['username']
+    username= models.CharField(blank=True, max_length=254,)
+    email = models.EmailField(blank=False, max_length=254, unique=True, verbose_name="email address")
